1e1d08a0a8c18bd8817e859e320c0208c8314a7c
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2024-02-28 15:46:58 -0300

    chore: switch to hash router

--- projects/admin/src/router/index.ts
+++ projects/admin/src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   linkActiveClass: 'active',
   routes: [
     { path: '/', component: () => import('../views/HomeView.vue') },
