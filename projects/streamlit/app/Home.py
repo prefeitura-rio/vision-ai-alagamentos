bbb49248653d1b4e263e09ceaffdfd335306de73
d116626 <d116626@gmail.com>
2024-03-21 10:57:51 -0300

    chore: remove false from show critical images

--- projects/streamlit/app/Home.py
+++ projects/streamlit/app/Home.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # import folium # noqa
 
-import streamlit as st
 from streamlit_folium import st_folium  # noqa
 from utils.utils import (
     create_map,
@@ -13,6 +12,8 @@ from utils.utils import (
     get_icon_color,
     treat_data,
 )
+
+import streamlit as st
 
 st.set_page_config(page_title="Vision AI - Rio", layout="wide", initial_sidebar_state="collapsed")
 # st.image("./data/logo/logo.png", width=300)
@@ -174,7 +175,7 @@ if not all(x is None for x in cameras_identifications):
             column = _col1 if i % 2 == 0 else _col2
 
             with column:
-                if str(row["label"]) not in ["null", "free", "low"]:
+                if str(row["label"]) not in ["null", "free", "low", "false"]:
                     st.markdown(
                         f"### Camera ID: {str(row['id'])}"
                     )  # Ensure to convert to string if needed
