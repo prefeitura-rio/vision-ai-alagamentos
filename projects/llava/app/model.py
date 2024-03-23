3b810291b1ba3379ec0602a8e9f4870b4ca90f90
Thiago Felipe <thiago.felipe@tutanota.com>
2024-03-23 08:28:12 +0000

    feat: adding batch inferances

--- projects/llava/app/model.py
+++ projects/llava/app/model.py
@@ -3,13 +3,17 @@ import logging
 import os
 import threading
 import traceback
+import time
+import uuid
 from argparse import ArgumentParser
 from dataclasses import dataclass
 from io import BytesIO
+from typing import Any
 
 import requests
 import torch
 import zmq
+from diskcache import Cache
 from PIL import Image
 from transformers import (
     BitsAndBytesConfig,
@@ -56,7 +60,7 @@ class ModelParams:
     max_length: int
     do_sample: bool
 
-    def __init__(self, params):
+    def __init__(self, params: dict[str, Any]):
         self.temperature = float(params.get("temperature", 0.2))
         self.top_p = float(params.get("top_p", 1.0))
         self.top_k = int(params.get("top_k", 32))
@@ -84,17 +88,16 @@ class Model:
             )
         )
         self.model = model
-        logging.info("Model loaded")
+        logging.debug("Model loaded")
 
         processor: LlavaNextProcessor = LlavaNextProcessor.from_pretrained(
             model_name,
             use_fast=False,
         )
         self.processor = processor
-        logging.info("Processor loaded")
+        logging.debug("Processor loaded")
 
-    # TODO: fazer inferencia por batch
-    def generate_output(self, prompt: str, images: list[Image.Image], params: ModelParams):
+    def generate_output(self, prompt: str | list[str], images: list[Image.Image], params: ModelParams):
         model, processor = self.model, self.processor
 
         inputs = processor(text=prompt, images=images, return_tensors="pt").to("cuda")
@@ -109,41 +112,134 @@ class Model:
             use_cache=True,
         )
 
-        return processor.batch_decode(output_ids, skip_special_tokens=True)[0].strip()
+        return [result.strip() for result in processor.batch_decode(output_ids, skip_special_tokens=True)]
 
 
-# TODO: criar uma pipline de processamento em batch
-# 1. validar parametros
-# 2. baixar imagem
-# 3. puxar parametros para fila de requisições
-# 4. retornar id da requisição
-# 5. ficar verificando reposta da requisição em um cache de memória
-def worker(worker_url: str, model: Model, context=None):
+def send_time(url_timer: str, sleep: int, context: zmq.Context | None = None):
     context = context or zmq.Context.instance()
+
+    socket = context.socket(zmq.PAIR)
+    socket.connect(url_timer)
+
+    while True:
+        time.sleep(sleep)
+        socket.send_string("ping")
+        socket.recv_string()
+
+
+def queue(queue_url: str, model: Model, params: ModelParams, cache: Cache, context: zmq.Context | None = None):
+    context = context or zmq.Context.instance()
+    queue = []
+    last_time = time.time()
+
+    socket = context.socket(zmq.REP)
+    socket.connect(queue_url)
+
+    url_timer = f"inproc://timer-{uuid.uuid4()}"
+    timer = context.socket(zmq.PAIR)
+    timer.bind(url_timer)
+
+    thread_timer = threading.Thread(target=send_time, args=(url_timer, 1))
+    thread_timer.daemon = True
+    thread_timer.start()
+
+    poller = zmq.Poller()
+    poller.register(timer, zmq.POLLIN)
+    poller.register(socket, zmq.POLLIN)
+
+    while True:
+        try:
+            socks = dict(poller.poll())
+
+            if socks.get(timer) == zmq.POLLIN:
+                timer.recv_string()
+                timer.send_string("pong")
+                logging.debug("timer ping")
+
+            if socks.get(socket) == zmq.POLLIN:
+                message = socket.recv_pyobj()
+                logging.debug(f"received queue request: {message}")
+                id = uuid.uuid4()
+                queue.append((id, message))
+                socket.send_pyobj(id)
+
+            now = time.time()
+
+            if len(queue) >= 5 or (now - last_time >= 10 and len(queue) > 0):
+                prompts = []
+                images = []
+                ids = []
+
+                for id, message in queue:
+                    prompts.append(message["prompt"])
+                    images += message["images"]
+                    ids.append(id)
+
+                logging.debug("starting batch inferance")
+                responses = model.generate_output(prompts, images, params)
+
+                for id, response in zip(ids, responses):
+                    logging.debug(f"complete inferance: {id}")
+                    cache.set(id, response, expire=120)
+
+                queue = []
+
+            if now - last_time >= 10:
+                logging.debug(f"queue size: {len(queue)}")
+                last_time = now
+
+
+        except Exception as e:
+            logging.error("Caught Unknown Error", e)
+            logging.error(traceback.format_exc())
+
+
+def worker(worker_url: str, queue_url: str, cache: Cache, context: zmq.Context | None = None):
+    context = context or zmq.Context.instance()
+    max_retries = 200
 
     socket = context.socket(zmq.REP)
     socket.connect(worker_url)
 
-    while True:
-        message = socket.recv_pyobj()
-        print(f"Received request: {message}")
+    queue = context.socket(zmq.REQ)
+    queue.connect(queue_url)
 
+    while True:
         try:
-            prompt, images = parser_prompt(message)
-            params = ModelParams(message)
-            text = model.generate_output(prompt, images, params)
-            error_code = 0
-        except ValueError as e:
-            logging.error("Caught ValueError:", e)
-            text = e
-            error_code = 1
+            message = socket.recv_pyobj()
+            logging.debug(f"Received worker request: {message}")
+
+            try:
+                prompt, images = parser_prompt(message)
+                queue.send_pyobj({"prompt": prompt, "images": images})
+                id = queue.recv_pyobj()
+                text = cache.get(id)
+                retries = 0
+
+                while text is None and retries <= max_retries:
+                    time.sleep(1)
+                    retries += 1
+                    text = cache.get(id)
+
+                error_code = 0
+                if text is None:
+                    logging.error("Inferance Timeout")
+                    text = "Inferance Timeout"
+                    error_code = 3
+            except ValueError as e:
+                logging.error("Caught ValueError:", e)
+                text = e
+                error_code = 1
+            except Exception as e:
+                logging.error("Caught Unknown Error", e)
+                logging.error(traceback.format_exc())
+                text = "Internal Server Error"
+                error_code = 2
+
+            socket.send_pyobj({"text": text, "error_code": error_code})
         except Exception as e:
             logging.error("Caught Unknown Error", e)
             logging.error(traceback.format_exc())
-            text = "Internal Server Error"
-            error_code = 2
-
-        socket.send_pyobj({"text": text, "error_code": error_code})
 
 
 if __name__ == "__main__":
@@ -152,26 +248,69 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
-    logging.info("Starting model")
+    logging.debug("Starting model")
     model = Model()
+
     url_worker = "inproc://workers"
     url_client = args.listen_address
+    url_queue_router = "inproc://queues_router"
+    url_queue_dealer = "inproc://queues_dealer"
 
     context = zmq.Context.instance()
 
     clients = context.socket(zmq.ROUTER)
     clients.bind(url_client)
+    logging.debug("Binded clients")
 
     workers = context.socket(zmq.DEALER)
     workers.bind(url_worker)
+    logging.debug("Binded workers")
 
-    for i in range(5):
-        thread = threading.Thread(target=worker, args=(url_worker, model))
+    queues_router = context.socket(zmq.ROUTER)
+    queues_router.bind(url_queue_router)
+    logging.debug("Binded queues router")
+
+    queues_dealer = context.socket(zmq.DEALER)
+    queues_dealer.bind(url_queue_dealer)
+    logging.debug("Binded queues dealer")
+
+    cache = Cache()
+
+    for _ in range(10):
+        thread = threading.Thread(target=worker, args=(url_worker, url_queue_router, cache))
         thread.daemon = True
         thread.start()
+    logging.info("Workers started")
 
-    zmq.proxy(clients, workers)
+    for _ in range(1):
+        thread = threading.Thread(target=queue, args=(url_queue_dealer, model, ModelParams({}), cache))
+        thread.daemon = True
+        thread.start()
+    logging.info("Queues started")
 
-    clients.close()
-    workers.close()
-    context.term()
+
+    logging.info("starting polling")
+    poller = zmq.Poller()
+    poller.register(clients, zmq.POLLIN)
+    poller.register(workers, zmq.POLLIN)
+    poller.register(queues_router, zmq.POLLIN)
+    poller.register(queues_dealer, zmq.POLLIN)
+
+    while True:
+        socks = dict(poller.poll())
+
+        if socks.get(clients) == zmq.POLLIN:
+            logging.debug("sending message to worker")
+            workers.send_multipart(clients.recv_multipart())
+
+        if socks.get(workers) == zmq.POLLIN:
+            logging.debug("sending message to client")
+            clients.send_multipart(workers.recv_multipart())
+
+        if socks.get(queues_router) == zmq.POLLIN:
+            logging.debug("sending message to queue dealer")
+            queues_dealer.send_multipart(queues_router.recv_multipart())
+
+        if socks.get(queues_dealer) == zmq.POLLIN:
+            logging.debug("sending message to queue router")
+            queues_router.send_multipart(queues_dealer.recv_multipart())
